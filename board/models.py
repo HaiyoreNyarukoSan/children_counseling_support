b0925e442418c53e46e99d608600e93266a2b9ad
HaiyoreNyarukoSan <progogo12@gmail.com>
2023-08-18 10:59:17 +0900

    feat : Add ORM for board

---
+++ board/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+
+from users.models import Patient, Counselor, User
+
+
+# Create your models here.
+class Article(models.Model):
+    a_patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    a_title = models.CharField(max_length=200)
+    a_content = models.CharField(max_length=200)
+    # 글 작성일
+    a_published_date = models.DateTimeField('date published', auto_now_add=True)
+    # 상담용 그림들
+    a_tree_image = models.ImageField("나무 이미지", upload_to='htp/tree', blank=True)
+    a_man_image = models.ImageField("남자사람 이미지", upload_to='htp/man', blank=True)
+    a_woman_image = models.ImageField("여자사람 이미지", upload_to='htp/woman', blank=True)
+    a_house_image = models.ImageField("집 이미지", upload_to='htp/house', blank=True)
+
+    def __str__(self):
+        return self.a_title
+
+
+class Comment(models.Model):
+    c_commenter = models.ForeignKey(User, on_delete=models.CASCADE)
+    c_content = models.CharField(max_length=200)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.c_content
+
+
+class CounselorReview(models.Model):
+    r_patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    r_counselor = models.ForeignKey(Counselor, on_delete=models.CASCADE)
+    r_rating = models.FloatField()
+
+    def __str__(self):
+        return f'{self.r_patient} rated {self.r_counselor} {self.r_rating}'
--- children_counseling_support/urls.py
+++ children_counseling_support/urls.py
@@ -13,9 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.views.generic import TemplateView
+
+from children_counseling_support import settings
 
 urlpatterns = [
     path("", TemplateView.as_view(template_name='Main.html'), name='home'),
@@ -30,3 +33,9 @@ urlpatterns = [
     path("Picture-list", TemplateView.as_view(template_name='Picture-list.html'), name='Picture-list'),
     path("admin/", admin.site.urls),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        prefix=settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT,
+    )

7570f993cfc2f3ccc8c1e0b33b509a76564d3d6f
youngjinjae <wh7045@gmail.com>
2023-08-28 16:22:59 +0900

    chat 폴더 진행중

---
+++ chat/views.py
@@ -0,0 +1,15 @@
+# chat/views.py
+from django.shortcuts import render
+
+from chat.models import chat_room
+
+
+def room(request):
+    article = None
+    patient = article.a_patient if article else None
+    counselor = None
+    room, _ = chat_room.objects.get_or_create(r_article=article, r_counselor=counselor, r_patient=patient)
+    room.chat_message_set.all()
+    room = chat_room()
+    context = {'room': room}
+    return render(request, "chat/room.html", context)

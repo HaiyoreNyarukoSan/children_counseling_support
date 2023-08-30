f6f2ec7a7bfe54726b723f8c3eef57f47c7b734f
HaiyoreNyarukoSan <progogo12@gmail.com>
2023-08-30 09:33:06 +0900

    feat : add chatting room list page

--- chat/urls.py
+++ chat/urls.py
@@ -8,4 +8,6 @@ app_name = 'chat'
 urlpatterns = [
     path("patient", views.room_patient, name="patient_room"),
     path("counselor", views.room_counselor, name="counselor_room"),
+    path("get_room/<int:room_id>", views.get_room, name="get_room"),
+    path("my_rooms", views.chat_room_list, name="my_rooms"),
 ]
